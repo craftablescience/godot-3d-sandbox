063adec45a422a4c0b817f0a35cfce0abdb28bbe
Brendan Lewis <brendanelewis@outlook.com>
2019-12-11 19:35:29 -0500

    Added loading indicator

--- scripts/MainMenuHandler.gd
+++ scripts/MainMenuHandler.gd
@@ -1,9 +1,10 @@
 extends Control
 
-
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-	for button in $VBoxContainer.get_children():
+	$HBoxContainer/VBoxContainer2/ProgressBar.hide() # TODO
+	$HBoxContainer/VBoxContainer2.hide()
+	for button in $HBoxContainer/VBoxContainer.get_children():
 		if (button is Button):
 			button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
 
@@ -11,4 +12,5 @@ func _on_Button_pressed(scene_to_load : String):
 	if (scene_to_load == "QUIT"):
 		get_tree().quit()
 	else:
+		$HBoxContainer/VBoxContainer2.show()
 		get_tree().change_scene(scene_to_load)
